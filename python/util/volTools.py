571c131c4516d34bb5dc1c11742481271212aac9
d-v-b <vaslav.tchitcherine@gmail.com>
2015-06-02 18:29:48 -0400

    added vidEmbed to volTools.py

--- python/util/volTools.py
+++ python/util/volTools.py
@@ -58,6 +58,15 @@ def getStackData(rawPath, channel=0, frameNo=0):
     im = im.reshape(dims[-1::-1])
     return im
 
+def vidEmbed(fname, mimetype):
+    """Load the video in the file `fname`, with given mimetype, and display as HTML5 video.
+    Credit: Fernando Perez
+    """
+    from IPython.display import HTML
+    video_encoded = open(fname, "rb").read().encode("base64")
+    video_tag = '<video controls alt="test" src="data:video/{0};base64,{1}">'.format(mimetype, video_encoded)
+    return HTML(data=video_tag)
+
 
 def volumeMask(vol):
     """
