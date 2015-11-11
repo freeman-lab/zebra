c90a71a8ccb871e0eef50badf21690adecaa3f97
d-v-b <vaslav.tchitcherine@gmail.com>
2015-11-11 14:52:20 -0500

    fixed bug in smoothpower function

--- python/util/epTools.py
+++ python/util/epTools.py
@@ -84,7 +84,7 @@ def smoothPower(ch, kern=None):
     """
     from scipy import signal as sig
 
-    if not kern:
+    if kern is None:
         kern = sig.gaussian(121, 20)
         kern = kern/kern.sum()
 
